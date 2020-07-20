2daad993bdf35a936e0dc53a843b1605ffaadf87
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-07-20 19:16:31 -0300

    Preparacao Inicial

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection connection = DB.getConnection();
+		DB.closeConnection();
+		
+
+	}
+
+}
