23378ff70bba819bb8719940b366bcc9707d0df8
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-07-21 13:36:17 -0300

    Implementação Atualizar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -66,6 +66,27 @@ public class Program {
 		 * 
 		 * finally { DB.closeStatement(preparedStatement); DB.closeConnection(); }
 		 */
+		
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		
+		/*UPDATE
+		 * Connection connection = null; PreparedStatement preparedStatement = null;
+		 * 
+		 * try { connection = DB.getConnection(); preparedStatement =
+		 * connection.prepareStatement( "UPDATE seller " +
+		 * "SET BaseSalary = BaseSalary + ? " + "WHERE" + "(DepartmentId = ?)");
+		 * 
+		 * preparedStatement.setDouble(1, 200); preparedStatement.setInt(2, 2);
+		 * 
+		 * int rowsAffected = preparedStatement.executeUpdate();
+		 * 
+		 * System.out.println("Done! Rows Affected: " + rowsAffected);
+		 * 
+		 * } catch (SQLException e) { e.printStackTrace(); } finally {
+		 * DB.closeStatement(preparedStatement); DB.closeConnection(); }
+		 */
+		
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 	}
 
 }
